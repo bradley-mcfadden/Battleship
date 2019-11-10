6df6c610bd3741eec21aa909b7dbac5c2bc8093e
Bradley McFadden <bmcfadden00@gmail.com>
2019-11-10 02:45:54 -0500

    Formatted the specifications in a better way

--- Board.cpp
+++ Board.cpp
@@ -1,22 +1,23 @@
+//Bradley McFadden
+//SN 0370776
+
 #include "Board.h"
-//reset board to EMPTY state
+//Reset all boards to EMPTY pieces
 void Board::reset() noexcept{
 	for (size_t idx; idx < BOARD_SIZE; ++idx){
-		m_primary1[idx] = m_primary2[idx] 
-		= PlayerPiece::EMPTY;
-		m_tracking1[idx] = m_tracking2[idx] 
-		= EnemyPiece::EMPTY;
+		m_primary1[idx] = m_primary2[idx] = PlayerPiece::EMPTY;
+		m_tracking1[idx] = m_tracking2[idx] = EnemyPiece::EMPTY;
 	}
 	totalMoves = 0;
 }
 
-//verify a move is withing range and not targeting an
-//already attacked cell
+//Verify that a move issued during regular turns is within
+//bounds and not attacking a HIT/MISS cell
+//<param move>Move to verify
+//<return>Is the move indeed legal?
 bool Board::isLegal(const Move &move) const noexcept{
 	//validate range
-	if (!(move.row >= 1 && move.row <= 10))
-		return false;
-	if (!(move.col >= 'A' && move.col <= 'J'))
+	if (!isWithinRange(move))
 		return false;
 	//convert from x,y to xy
 	size_t x = (size_t)(move.col - 65);
@@ -39,18 +40,22 @@ bool Board::isLegal(const Move &move) const noexcept{
 	return true;
 }
 
-//output player's board and enemy's tracking
-//   A  B  C  D  E  F  G  H  I  J
-//1
-//2
-//3
-//4
-//5
-//6
-//7
-//8
-//9
-//10
+//Output player's board and enemy's tracking board 
+//to std::out
+//<param player> The player whose primary to print
+//<notes> the display of each piece is done using
+//an integer cast from enumerated types.
+//The following pieces correspond the the numbers
+//in the second column:
+//EnemyPiece::EMPTY		0
+//EnemyPiece::HIT		1
+//EnemyPiece::MISS		2
+//PlayerPiece::EMPTY		0
+//PlayerPiece::PATROL_BOAT	1
+//PlayerPiece::SUBMARINE	2
+//PlayerPiece::DESTROYER	3
+//PlayerPiece::BATTLESHIP	4
+//PlayerPiece::CARRIER		5
 void Board::display(int player) const noexcept{
 	EnemyPiece e_tracking[BOARD_SIZE];
 	PlayerPiece p_primary[BOARD_SIZE];
@@ -99,7 +104,9 @@ void Board::display(int player) const noexcept{
 	}
 }
 
-//update the correct board with the corresponding move
+//Updates tracking and primary boards after a move
+//<param move> What move to update the board with
+//<return>The EnemyPiece at the corresponding index
 EnemyPiece Board::makeMove(const Move &move){
 	//convert x,y to xy
 	size_t x = (size_t)(move.col - 65);
@@ -133,3 +140,89 @@ EnemyPiece Board::makeMove(const Move &move){
 	}	
 }
 
+//Take in two moves as the start and end point of a piece you wanna place on the board
+//<param move1> Start of the piece
+//<param move2> End of the piece
+//<param player> Which player is placing the piece?
+//<param piece> What piece does the player want to place?
+//<return> True if successful, false if not
+bool Board::placePiece(const Move &move1, const Move& move2, int player, PlayerPiece piece) 
+noexcept{
+	//bounds check player and move parameters
+	if (player > 2 || player < 1)
+		return false;
+	if (!(isWithinRange(move1) && isWithinRange(move2)))
+		return false;
+		
+	//convert moves into integers, then to index
+	size_t x1 = (size_t)(move1.col - 65);
+	size_t y1 = (move1.row - 1);
+	size_t idx1 = x1 + y1 * 10;
+
+	size_t x2 = (size_t)(move2.col - 65);
+	size_t y2 = (move2.row - 1);
+	size_t idx2 = x2 + y2 * 10;
+	
+	//determine the distance between both points
+	//and the size of the boat to place
+	size_t euclideanDist = x1 - x2 + y1 - y2;
+	size_t boatSize;
+	switch(piece){
+	case PlayerPiece::PATROL_BOAT:
+		boatSize = 2;
+		break;
+	case PlayerPiece::SUBMARINE:
+		boatSize = 3;
+		break;
+	case PlayerPiece::DESTROYER:
+		boatSize = 3;
+		break;
+	case PlayerPiece::BATTLESHIP:
+		boatSize = 4;
+		break;
+	case PlayerPiece::CARRIER:
+		boatSize = 5;
+		break;
+	default:
+		return false;
+	}
+	if (euclideanDist != boatSize)
+		return false;
+
+	PlayerPiece *temp;
+	temp = (player == 1 ? m_primary1 : m_primary2);
+
+	//figure out whether to move up, down, right or left
+	//across the grid when assigning elements
+	int step = 0;
+	if (idx1 == idx2){
+		return false;
+	} else if (x1 == x2){
+		step = (x1 < x2 ? 10 : -10);
+	} else if (y1 == y2){
+		step = (y1 < y2 ? 1 : -1);
+	} else {
+		return false;
+	}
+
+	//update each grid cell with new piece
+	for (size_t i=idx1; i<idx2; i+=step){
+		if (temp[i] != PlayerPiece::EMPTY)
+			return false;
+		temp[i] = piece;
+	}
+	return true;
+}
+
+//Checks if a Move object is within the bounds
+//of the board
+//<param move>The move to check
+//<return>True if within bounds
+bool Board::isWithinRange(const Move &move)
+const noexcept{
+	if (!(move.row >= 1 && move.row <= 10))
+		return false;
+	if (!(move.col >= 'A' && move.col <= 'J'))
+		return false;
+	return true;
+}
--- Board.h
+++ Board.h
@@ -8,21 +8,22 @@ struct CharInt{
 	size_t row;
 	char col;
 };
+
 typedef CharInt Move;
 
 enum class EnemyPiece{
+	EMPTY,
 	HIT,
-	MISS,
-	EMPTY
+	MISS
 };
 
 enum class PlayerPiece{
-	PATROL_BOAT,
-	SUBMARINE,
-	DESTROYER,
-	BATTLESHIP,
-	CARRIER,
-	EMPTY
+	EMPTY,
+	PATROL_BOAT,//2
+	SUBMARINE,//3
+	DESTROYER,//3
+	BATTLESHIP,//4
+	CARRIER//5
 };
 
 class Board{
@@ -36,23 +37,30 @@ class Board{
 public:
 	//set every board to EMPTY and totalMoves to 0
 	Board(){ reset(); }
+
 	//set every board to EMPTY and totalMoves to 0
 	void reset() noexcept;
+
 	//output p1 board and p2 tracking if player 1
 	//else output the opposite
 	void display(int player) const noexcept;
+
 	//verify that a move's coordinates are within
 	//[A-J] and [1-10] range, and also that they 
 	//do not target an already attacked cell
 	bool isLegal(const Move &move) const noexcept;
+
 	//updates the board with a move and returns
 	//state at the corresponding cell
 	EnemyPiece makeMove(const Move &move);
-	//returns false if piece goes off board
-	//or overlaps with another piece
-	bool isLegal(const Move &move, const Move &move) const noexcept;
+
 	//place a piece on the board
 	//must check if the move is legal
-	PlayerPiece placePiece(const Move &move, const Move &move, PlayerPiece what) const noexcept;
+	bool placePiece(const Move &move1, const Move &move2,
+	int player, PlayerPiece piece) noexcept;
+
+	//bounds checks a move
+	bool isWithinRange(const Move &move) 
+	const noexcept;
 };
 #endif
